2699a0fa70dce776aaea28f4f5862a2ef79ab824
Edson-Emanoel <edson.emanoelofc@gmail.com>
2024-08-10 18:37:19 -0300

    Começei o Sidebar do Web

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -7,6 +7,12 @@ export function Sidebar() {
       return(
        <div className="flex w-full flex-col bg-muted/40">
             
+            <aside
+                  className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex"
+            >
+                  
+            </aside>
+
             <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
                   <header
                         className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6"
